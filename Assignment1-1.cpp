2cfbc1a0d15020c215029d80ca6b3051e475fd85
mazinjam <mazinjam1@gmail.com>
2021-09-07 20:02:54 -0700

    commit test

---
+++ Assignment1-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Student {
+  int id;
+  string sname;
+  double score[2];
+  double sum;
+  double avg;
+};
+
+int main()
+{
+  ifstream ifs;
+  Student s;
+  ifs.open("student.bin");
+  ifs.read( (char *)&s, sizeof(s));
+  cout << "Id " << s.id << "\t";
+  cout << "Name " << s.sname << "\t";
+  cout << "Score1 " << s.score[0] << "\t";
+  cout << "Score2 " << s.score[1] << "\t";
+  cout << "Sum " << s.sum << "\t";
+  cout << "Avg " << s.avg << "\t";
+  //hi
+  
+
+}
